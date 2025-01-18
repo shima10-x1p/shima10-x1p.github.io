94a5db016e28113ded9c0331707aebe3b56335ea
shima10-x1p <moto20000203@gmail.com>
2025-01-18 22:36:29 +0900

    画像の読み込み方変更

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import './App.css'
 import NavBar from './components/NavBar'
 import InfoCard from './components/InfoCard'
+import xIcon from './assets/twitter.svg'
+import githubIcon from './assets/github.svg'
+import editIcon from './assets/edit.svg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,9 +13,9 @@ function App() {
     <>
       <NavBar />
       <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3'>
-        <InfoCard title={"X"} icon_src={"/src/assets/twitter.svg"} link={"https://x.com/shima10_x1p"}/>
-        <InfoCard title={"Github"} icon_src={"/src/assets/github.svg"} link={"https://github.com/shima10-x1p"}/>
-        <InfoCard title={"Zenn"} icon_src={"/src/assets/edit.svg"} link={"https://zenn.dev/shima10_x1p"}/>
+        <InfoCard title={"X"} icon_src={xIcon} link={"https://x.com/shima10_x1p"}/>
+        <InfoCard title={"Github"} icon_src={githubIcon} link={"https://github.com/shima10-x1p"}/>
+        <InfoCard title={"Zenn"} icon_src={editIcon} link={"https://zenn.dev/shima10_x1p"}/>
       </div>
     </>
   )
