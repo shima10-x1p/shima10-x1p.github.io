4aeffc6fae756d4d4b9225e8cf5ac461236963bb
shima10-x1p <moto20000203@gmail.com>
2025-01-19 22:45:05 +0900

    X -> Twitter

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     <>
       <NavBar />
       <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3'>
-        <InfoCard title={"X"} icon_src={xIcon} link={"https://x.com/shima10_x1p"} />
+        <InfoCard title={"Twitter（現X）"} icon_src={xIcon} link={"https://x.com/shima10_x1p"} />
         <InfoCard title={"Github"} icon_src={githubIcon} link={"https://github.com/shima10-x1p"} />
         <InfoCard title={"Zenn"} icon_src={editIcon} link={"https://zenn.dev/shima10_x1p"} />
       </div>
